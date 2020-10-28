b9317cccc4bbcda5fa33ea614a038278bfd41556
goharAg <aghajanyan.gohar2903@gmail.com>
2020-10-28 21:53:54 +0400

    addfirst file DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public  class DataTypes{
+    public static void main(String[] args) {
+        byte a = 45;
+        short b = 1277;
+        int c = 7899;
+        long bigNumer = 45454545454545643213131L;
+        float floatNum= 15.3f;
+        double doubleNum = 789.65;
+        boolean isReady = true;
+        char mySymbol = '*';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(bigNumer);
+        System.out.println(floatNum);
+        System.out.println(doubleNum);
+        System.out.println(isReady);
+        System.out.println(mySymbol);
+
+    }
+}
